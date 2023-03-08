f3f4a6ea43e8e1ff9ed8e7c686ddf1c24cd6ea2f
unknown <arminfrontend@gmail.com>
2023-03-08 21:58:58 +0330

    Feat: Created List component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,14 @@
+import List from "../components/List";
+
 const Home = () => {
     return (
         <main>
             {/* main container  */}
             <div className="main-container">
-                
+                {/* content container  */}
+                <div className="flex space-x-3">
+                <List/>
+                </div>
             </div>
         </main>
     )
---
+++ src/components/List.jsx
@@ -0,0 +1,10 @@
+
+const List = () => {
+    return (
+        <div className="flex flex-col w-1/3">
+            <h1>List</h1>
+        </div>
+    )
+}
+
+export default List;
