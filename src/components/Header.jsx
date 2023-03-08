2c8968c0a328af7c8476fd88894cdba582e669f2
unknown <arminfrontend@gmail.com>
2023-03-08 17:02:06 +0330

    Feat: Added content container

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,11 @@ const Header = () => {
             <div className="container mx-auto px-6 py-2">
                 {/* Navbar */}
                 <nav className="flex justify-between items-center">
+                    {/* Logo  */}
+                    <h5>Travel Advisor</h5>
+                    {/* content container  */}
+                    <div className="flex items-center space-x-3">
+                    </div>
 
                 </nav>
             </div>
