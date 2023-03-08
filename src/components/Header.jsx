0fd0afd211bca80ad85a4910c7f776ea12ca0c66
unknown <arminfrontend@gmail.com>
2023-03-08 16:56:50 +0330

    Feat: Added Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,10 @@ const Header = () => {
         <header className="bg-gradient-to-r from-violet-700 to-indigo-600 text-white">
             {/* main container  */}
             <div className="container mx-auto px-6 py-2">
+                {/* Navbar */}
+                <nav className="flex justify-between items-center">
+
+                </nav>
             </div>
 
         </header>
