9d6199a79d9acbecb544ded74b9cf2b6d912e186
unknown <arminfrontend@gmail.com>
2023-03-08 16:53:42 +0330

    Added main container to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 
 const Header = () => {
     return (
+        // Header
         <header className="bg-gradient-to-r from-violet-700 to-indigo-600 text-white">
+            {/* main container  */}
+            <div className="container mx-auto px-6 py-2">
+            </div>
+
         </header>
     )
 }
