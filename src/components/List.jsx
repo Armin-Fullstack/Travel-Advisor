7213b4fd3dbb0e464524525e54b0a155258b9244
unknown <arminfrontend@gmail.com>
2023-03-09 22:25:32 +0330

    Feat: Implemented state for type and rating

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 
 const List = () => {
+    const [type , setType] = useState("Restaurant")
+    const [rating , setRating] = useState("All")
     return (
         <div className="flex flex-col flex-1">
             <h4 className="text-center mt-5 font-bold tracking-wide">Restaurants, Hotels & Attractions around you</h4>
@@ -8,16 +11,16 @@ const List = () => {
 
                 <form className="flex flex-col space-y-1">
                     <label htmlFor="places" className="text-xs text-stone-500">Type</label>
-                    <select name="places" className="text-xs focus:outline-none pr-3 pb-1 border-b border-slate-400">
+                    <select value={type} onChange={(e) => setType(e.target.value)} name="places" className="text-xs focus:outline-none pr-3 pb-1 border-b border-slate-400">
                         <option value="Restaurants">Restaurants</option>
-                        <option value="Restaurants">Hotels</option>
-                        <option value="Restaurants">Attractions</option>
+                        <option value="Hotels">Hotels</option>
+                        <option value="Attractions">Attractions</option>
                     </select>
                 </form>
 
                 <form className="flex flex-col space-y-1">
                     <label htmlFor="places" className="text-xs text-stone-500">Rating</label>
-                    <select name="places" className="text-xs focus:outline-none pr-3 pb-1 border-b border-slate-400">
+                    <select value={rating} onChange={e => setRating(e.target.value)} name="places" className="text-xs focus:outline-none pr-3 pb-1 border-b border-slate-400">
                         <option value={0}>All</option>
                         <option value={3}>Above 3.0</option>
                         <option value={4}>Above 4.0</option>
