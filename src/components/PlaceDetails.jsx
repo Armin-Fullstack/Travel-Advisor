85668b7447e3af3380414c3d988ec4c1af17be54
unknown <arminfrontend@gmail.com>
2023-03-12 00:43:01 +0330

    Feat: Added title of content

--- src/components/PlaceDetails.jsx
+++ src/components/PlaceDetails.jsx
@@ -3,10 +3,14 @@ const PlaceDetails = ({restaurant}) => {
     return (
         
         // cards container 
-        <div className="flex flex-col space-y-3 mt-4 bg-slate-400">
+        <div className="flex flex-col space-y-3 mt-4">
             {/* card container  */}
-            <div className="w-full space-y-3">
-                <img src={restaurant.photo ? restaurant.photo.images.large.url : ""} alt="" className="h-[350px] w-full"/>
+            <div className="w-full border rounded-t-md shadow-md space-y-1">
+                <img src={restaurant.photo ? restaurant.photo.images.large.url : ""} alt="" className="rounded-t-md h-[350px] w-full"/>
+                {/* content container  */}
+                <div className="space-y-2 p-2">
+                    <h5>{restaurant.name}</h5>
+                </div>
             </div>
         </div>
     )
