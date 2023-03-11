6b5900599e4c1e0942d42205752b34e4dcff7fac
unknown <arminfrontend@gmail.com>
2023-03-12 00:26:48 +0330

    Feat: Rendering image cards

--- src/components/PlaceDetails.jsx
+++ src/components/PlaceDetails.jsx
@@ -4,7 +4,10 @@ const PlaceDetails = ({restaurant}) => {
         
         // cards container 
         <div className="flex flex-col space-y-3 mt-4 bg-slate-400">
-            
+            {/* card container  */}
+            <div className="w-full space-y-3">
+                <img src={restaurant.photo ? restaurant.photo.images.large.url : ""} alt="" className="h-[350px] w-full"/>
+            </div>
         </div>
     )
 }
